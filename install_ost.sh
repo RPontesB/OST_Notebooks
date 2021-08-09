90614a6d8d139ba55fe9ba6cef5ecbabbf5894ec
Ricardo Pontes <ricardo.bonfiglioli@agronow.com.br>
2021-08-09 17:09:00 -0300

    Update OST version

--- install_ost.sh
+++ install_ost.sh
@@ -4,8 +4,8 @@
 apt-get -y install python3-pip git libgdal-dev python3-gdal libspatialindex-dev wget libgfortran3 imagemagick file
 
 # set Snap and OTB version and download link
-OTB_VERSION="7.2.0"
-TBX_VERSION="7"
+OTB_VERSION="7.3.0"
+TBX_VERSION="8"
 TBX_SUBVERSION="0"
 SNAP_URL="http://step.esa.int/downloads/${TBX_VERSION}.${TBX_SUBVERSION}/installers"
 TBX="esa-snap_sentinel_unix_${TBX_VERSION}_${TBX_SUBVERSION}.sh"
@@ -13,10 +13,10 @@ TBX="esa-snap_sentinel_unix_${TBX_VERSION}_${TBX_SUBVERSION}.sh"
 # get Snap
 wget "${SNAP_URL}/${TBX}"
 chmod 755 ${TBX}
-wget https://raw.githubusercontent.com/ESA-PhiLab/OpenSarToolkit/master/snap7.varfile
+wget https://raw.githubusercontent.com/EOX-A/OpenSarToolkit/master/snap.varfile
 
 # install snap
-./${TBX} -q -varfile snap7.varfile
+./${TBX} -q -varfile snap.varfile
 rm ${TBX}
 
 # OTB installation
@@ -27,4 +27,4 @@ chmod +x $OTB
 rm -f ${OTB}
 
 # install ost
-pip install git+https://github.com/ESA-PhiLab/OpenSarToolkit
+pip install git+https://github.com/EOX-A/OpenSarToolkit
